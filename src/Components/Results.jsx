732547afe385d07e605877e795cdad70d7411494
Saj <Saj@Sayinthans-MacBook-Pro.local>
2020-05-08 17:19:35 -0400

    create Results component to render all results from from search

---
+++ src/Components/Results.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Employee from "./Employee"
+
+const Results = (props) => {
+  const { results } = props;
+
+  return results.map(result => {
+    return <Employee key={result.id} {...result} />;
+  });
+}
+
+export default Results;
