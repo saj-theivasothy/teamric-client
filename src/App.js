09a0a7713c6f3719c61fbf7ac4d990e2952e08ec
Nazia Shaik <42496130+shaiknazia@users.noreply.github.com>
2020-05-08 13:36:26 -0400

    Initial update

--- src/App.js
+++ src/App.js
@@ -1,62 +1,20 @@
-import React, { useState } from "react";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import sampleJSON from "./data/sample.json";
-
-import Sidebar from "./Components/Sidebar"
-
-import "antd/dist/antd.css";
-import "./index.css";
-import { Layout, Menu, Breadcrumb } from "antd";
-import {
-  MenuUnfoldOutlined,
-  MenuFoldOutlined,
-} from "@ant-design/icons";
-
-const { Header, Content, Footer } = Layout;
-
-const App = () => {
-  const [collapsedState, setCollapsedState] = useState(false);
-
-  const toggle = () => {
-    setCollapsedState(!collapsedState);
-  };
-
-  console.log(sampleJSON);
-
-  return (
-    <div className="App">
-      <Layout>
-      <Sidebar collapsedState={collapsedState}/>
-        <Layout className="site-layout">
-          <Header className="site-layout-background" style={{ padding: 0 }}>
-            {React.createElement(
-              collapsedState ? MenuUnfoldOutlined : MenuFoldOutlined,
-              {
-                className: "trigger",
-                onClick: toggle,
-              }
-            )}
-          </Header>
-          <Content
-            className="site-layout-background"
-            style={{
-              margin: "24px 16px",
-              padding: 24,
-              minHeight: 280,
-            }}
-          >
-            We need to create dynamic components here. For example...
-            <br />
-            profile content/viz gets rendered if user clicks on profile
-            assessment
-            <br />
-            content/viz gets rendered if user clicks on assessment
-          </Content>
-          <Footer style={{ textAlign: "center" }}>Teamric</Footer>
-        </Layout>
-        </Layout>
+function App() {
+  return <div>
+    <div class="sidenav">
+      <a href="#about">About</a>
+      <a href="#services">Services</a>
+      <a href="#clients">Clients</a>
+      <a href="#contact">Contact</a>
     </div>
-  );
-};
+    <div class="main">
+      <h2>TeamRic</h2>
+      <p>This sidebar is of full height (100%) and always shown.</p>
+    </div>
+</div>
+}
 
 export default App;
