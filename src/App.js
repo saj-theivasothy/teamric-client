fe17b3581c4215280393f5b5b14b55d24265d0ee
Nazia Shaik <42496130+shaiknazia@users.noreply.github.com>
2020-05-08 23:14:41 -0400

    added materials ui

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Sidebar from './Components/Sidebar';
+import FeedbackSummary from './Components/FeedbackSummary'
 
 function App() {
   return <div>
-    <div class="sidenav">
-      <a href="#about">About</a>
-      <a href="#services">Services</a>
-      <a href="#clients">Clients</a>
-      <a href="#contact">Contact</a>
-    </div>
-    <div class="main">
-      <h2>TeamRic</h2>
-      <p>This sidebar is of full height (100%) and always shown.</p>
-    </div>
+    <Sidebar/>
+    <FeedbackSummary/>
 </div>
 }
 
--- src/Components/FeedbackSummary.jsx
+++ src/Components/FeedbackSummary.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Sidebar() {
-return <div class="sidenav">
+return <div class="feedback">
 <a href="#about">About</a>
 <a href="#services">Services</a>
 <a href="#clients">Clients</a>
