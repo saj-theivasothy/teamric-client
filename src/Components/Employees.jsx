83561557985216a48c783668d461b05eb5ec4f54
Nazia Shaik <42496130+shaiknazia@users.noreply.github.com>
2020-05-18 16:46:52 -0400

    added employee page

--- src/Components/Employees.jsx
+++ src/Components/Employees.jsx
@@ -1,10 +1,40 @@
 import React from "react";
-import Graphic from "./Profile";
+import "./styles/employees.css";
+
+function EmployeeCard(props) {
+  return (
+    <section class="flip-card">
+      <div class="flip-card-inner">
+        <div class="flip-card-back">
+          <h1>{props.name}</h1>
+        </div>
+      </div>
+    </section>
+  );
+}
 
 function Employees() {
   return (
-    <div>
-      <h1>Employee page</h1>
+    <div className="main_heading">
+      <h3>PROFILE</h3>
+      <main className="main">
+        <section className="box1"></section>
+        <section className="box2">
+          {/* Box 1 orange color */}
+          <h1>Box2</h1>
+        </section>
+
+        {/* <h1>Box3</h1> */}
+
+        <EmployeeCard name="John Doe" />
+        <EmployeeCard name="John Doe" />
+        <EmployeeCard name="John Doe" />
+        <EmployeeCard name="John Doe" />
+        <EmployeeCard name="John Doe" />
+        <EmployeeCard name="John Doe" />
+        <EmployeeCard name="John Doe" />
+        <EmployeeCard name="John Doe" />
+      </main>
     </div>
   );
 }
